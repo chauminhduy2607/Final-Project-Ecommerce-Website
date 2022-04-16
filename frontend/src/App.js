4c03a2626ca73b2db75acd2faec0daa1ab601ee3
DuyChauMinh <chauminhduy.2607@gmail.com>
2022-04-17 04:10:47 +0700

    Test commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/"> TOKO </a>
+      </header>
+      <main>Products</main>
+    </div>
+  )
+}
+
+export default App
